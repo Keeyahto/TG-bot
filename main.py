e5ac98b3a217b1e26f639b814eb3f6ff10e32345
Yaroslav <ya.mironov14@gmail.com>
2022-01-25 17:17:47 +0300

    fix bugs

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-import asyncio
-
 from aiogram import Dispatcher
 from aiogram.types.bot_command import BotCommand
 from aiogram.utils.executor import start_webhook
 
 import commands as cmd
 from handlers import *
-from misc import dp, Bot, WEBHOOK_URL, WEBHOOK_PATH, WEBAPP_HOST, WEBAPP_PORT
+from misc import dp, bot, WEBHOOK_URL, WEBHOOK_PATH, WEBAPP_HOST, WEBAPP_PORT
 from models.base import init
 
 
@@ -21,7 +19,13 @@ async def set_commands(dp: Dispatcher):
     ])
 
 
-async def main(dp):
+async def on_shutdown(dp):
+    await bot.delete_webhook()
+    await dp.storage.close()
+    await dp.storage.wait_closed()
+
+
+async def on_startup(dp):
     # Регистрация хэндлеров
     register_handlers_common(dp)
     register_handlers_course(dp)
@@ -33,18 +37,25 @@ async def main(dp):
     # Запуск поллинга
     # await dp.skip_updates()  # пропуск накопившихся апдейтов (необязательно)
     await init()
-    await Bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True)
+    await bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True)
 
 
-if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main(dp))
-    loop.close()
+def main():
     start_webhook(
         dispatcher=dp,
         webhook_path=WEBHOOK_PATH,
         skip_updates=True,
-        on_startup=main,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
         host=WEBAPP_HOST,
         port=WEBAPP_PORT,
     )
+
+
+if __name__ == '__main__':
+    '''
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main(dp))
+    loop.close()
+    '''
+    main()
